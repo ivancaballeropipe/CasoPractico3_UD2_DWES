791f1a8dfe0492779dd4e345134f0dfc371d0f6c
Ivan <ivancaballeropipe@gmail.com>
2020-11-13 00:10:56 +0100

    primera subida

---
+++ adios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Iván Caballero Piñera</title> 
+    </head>
+    <body>
+        <h1><i><?php echo "Hasta luego desarrollador!!!"?></i></h1>
+    </body>
+</html>
